24f28850501e5815488dfbf54ccecd771e53d678
m7mdsamir20 <m7mdsamir20@gmail.com>
2026-04-28 17:40:26 +0300

    refactor: remove temporary migration and patch scripts from root directory

--- fix4.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const file = 'src/app/pos/page.tsx';
-let content = fs.readFileSync(file, 'utf8');
-
-content = content.replace("      </div>\r\n            )}", "");
-content = content.replace("      </div>\n            )}", "");
-
-fs.writeFileSync(file, content, 'utf8');
-console.log("Done");
